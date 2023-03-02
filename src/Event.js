0c6c112476d5eac38fb6373cbf8ab8ca8a4badaa
Steven <Briggs.steven369@gmail.com>
2023-03-01 19:29:58 -0800

    Add details area with Show/Hide Details button

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 
 class Event extends Component {
+  state = {extended: false};
+  handleClick = () => {
+    this.setState((prevState) => ({
+      extended: !prevState.extended,
+    }));
+
+  };
 
   render() {
     const {event} = this.props;
+    const {extended} = this.state;
 
     return <div className={'Event'}>
       <h1 className="summary">
@@ -11,6 +19,29 @@ class Event extends Component {
       </h1>
       <h2 className={'location'}>{event.location}</h2>
       <h2 className={'start-time'}>{event.start.dateTime}</h2>
+      <button
+          className={'details-button'}
+          onClick={() => this.handleClick()}>
+        {extended ? 'Hide' : 'Show'} Details
+      </button>
+      {extended && (
+          <div className='details'>
+            <h2>
+              More Details:
+            </h2>
+            <a
+              className='link'
+              href={event.htmlLink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              View Event Details on Google Calendar
+            </a>
+            <p className='description'>
+              {event.description}
+            </p>
+          </div>
+        )}
     </div>;
   }
 }
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import Event from '../Event';
 import {mockData} from '../mock-data';
 
@@ -32,6 +32,60 @@ describe('<Event /> component', () => {
       const startTime = EventWrapper.find('h2.start-time');
       expect(startTime).toHaveLength(1);
       expect(startTime.text()).toBe(event.start.dateTime);
+    });
+
+    test('the details button', () => {
+      const detailsButton = EventWrapper.find('button.details-button');
+      expect(detailsButton).toHaveLength(1);
+    });
+  });
+
+  describe('Details area', () => {
+
+    test('starts as collapsed with all extra details hidden', () => {
+      const detailsButton = EventWrapper.find('button.details-button');
+      expect(EventWrapper.state('extended')).toBe(false);
+      expect(detailsButton.text()).toBe('Show Details');
+
+      expect(EventWrapper.find('div.details')).toHaveLength(0);
+      expect(EventWrapper.find('a.link')).toHaveLength(0);
+      expect(EventWrapper.find('p.description')).toHaveLength(0);
+    });
+
+    test('expands when Show Details button is clicked', () => {
+      const EventWrapper = mount(<Event event={event}/>);
+      const detailsButton = EventWrapper.find('button.details-button');
+      detailsButton.simulate('click');
+
+      /*
+      * RETROSPECTIVE:
+      * Here was an interesting issue of this test was working as expected until I added a check to see if the button text was updating correctly
+      * All the details elements were displaying correctly but the button, according to the test was still saying 'Show Details'
+      * ChatGPT pointed out that if the test is checking the state immediately after clicking the button, the state update may not have had a chance to take effect yet
+      * The solution was to add a short timeout after clicking the button to allow all changes to take effect
+      * */
+      setTimeout(() => {
+        expect(EventWrapper.state('extended')).toBe(true);
+        expect(detailsButton.text()).toBe('Hide Details');
+
+        expect(EventWrapper.find('div.details')).toHaveLength(1);
+
+        expect(EventWrapper.find('a.link')).toHaveLength(1);
+        expect(EventWrapper.find('a.link').prop('href')).toBe(event.htmlLink);
+
+        expect(EventWrapper.find('p.description')).toHaveLength(1);
+        expect(EventWrapper.find('p.description').text()).
+            toBe(event.description);
+      }, 500);
+    });
+
+    test('collapses when Hide Details button is clicked', () => {
+      EventWrapper.setState({extended: true});
+
+      const detailsButton = EventWrapper.find('button.details-button');
+      detailsButton.simulate('click');
+
+      expect(EventWrapper.state('extended')).toBe(false);
     });
   });
 });
