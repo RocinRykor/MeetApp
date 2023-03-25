ead2bf3ceb8653c3e9d80998e6f09f767f42bee2
Steven <Briggs.steven369@gmail.com>
2023-03-25 11:27:45 -0700

    Add Alerts for CitySearch and NumberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+import {ErrorAlert} from './Alert';
 
 class NumberOfEvents extends Component {
-  state = {eventNum: 32};
+  state = {
+    eventNum: 32,
+    errorText: ''
+  };
 
   componentDidMount() {
     this.setState({eventNum: this.props.eventNum || 32});
@@ -10,12 +14,18 @@ class NumberOfEvents extends Component {
   changeEventNum(value) {
     this.setState({eventNum: value});
     this.props.updateNumberOfEvents(value);
+    if (value <= 0 || value > 64) {
+      this.setState({errorText: 'Invalid Input, please select a number between 1-64'})
+    } else {
+      this.setState({errorText: ''})
+    }
   }
 
   render() {
-    const {eventNum} = this.state;
+    const {eventNum, errorText} = this.state;
     return (
         <div className="numberOfEvents">
+          <ErrorAlert text={errorText}/>
           <h3>Number of Events:</h3>
           <input
               className="event-num-input"
