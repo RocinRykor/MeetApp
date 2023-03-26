3fa5daf35a5b876ae63ae5ea52f63eb6cfb6f3b5
Steven <Briggs.steven369@gmail.com>
2023-03-26 12:51:13 -0700

    Add welcome screen for Google auth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import {getEvents, extractLocations} from './api';
+import WelcomeScreen from './WelcomeScreen';
+import {getEvents, extractLocations, checkToken, getAccessToken} from
+      './api';
 import './nprogress.css';
 
 class App extends Component {
@@ -11,16 +13,23 @@ class App extends Component {
     events: [],
     locations: [],
     numberOfEvents: 32,
+    showWelcomeScreen: undefined,
   };
 
   async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        events = events.slice(0, this.state.numberOfEvents);
-        this.setState({events, locations: extractLocations(events)});
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = !(await checkToken(accessToken)).error;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    this.setState({showWelcomeScreen: !(code || isTokenValid)});
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({events, locations: extractLocations(events)});
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -28,24 +37,24 @@ class App extends Component {
   }
 
   updateEvents = (location = this.state.location, eventCount) => {
-  getEvents().then((events) => {
-    const locationEvents =
-      location === "all"
-        ? events
-        : events.filter((event) => event.location === location);
+    getEvents().then((events) => {
+      const locationEvents =
+          location === 'all'
+              ? events
+              : events.filter((event) => event.location === location);
 
-    const eventsToDisplay = eventCount || this.state.numberOfEvents;
-    const slicedEvents =
-      locationEvents.length === 0
-        ? events.slice(0, eventsToDisplay)
-        : locationEvents.slice(0, eventsToDisplay);
+      const eventsToDisplay = eventCount || this.state.numberOfEvents;
+      const slicedEvents =
+          locationEvents.length === 0
+              ? events.slice(0, eventsToDisplay)
+              : locationEvents.slice(0, eventsToDisplay);
 
-    this.setState({
-      events: slicedEvents,
-      location: location,
+      this.setState({
+        events: slicedEvents,
+        location: location,
+      });
     });
-  });
-};
+  };
 
   updateNumberOfEvents(number) {
     this.setState({
@@ -54,6 +63,9 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div
+        className="App"/>;
+
     return (
         <div className="App">
           <CitySearch locations={this.state.locations}
@@ -65,6 +77,8 @@ class App extends Component {
               updateEvents={this.updateEvents}
           />
           <EventList events={this.state.events}/>
+          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+                         getAccessToken={() => { getAccessToken(); }}/>
         </div>
     );
   }
